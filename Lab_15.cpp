cae1db449e2beeeac3872f9a14c5de6ac0a09746
Malakai <poncemalakai@gmail.com>
2025-09-28 21:20:12 -0700

    Commit 3 - the main functino with file management and while loop for getting al info from file

--- Lab_15.cpp
+++ Lab_15.cpp
@@ -18,4 +18,33 @@ public:
 
     const string& getTitle() const        { return title; }         // getters readable private objects
     int getYear() const                   { return yearReleased; }
-    const string& getScreenWriter() const
+    const string& getScreenWriter() const { return screenWriter; }
+
+    void print() const {
+        cout << "Movie: " << screenWriter << '\n'
+             << "  Year released: " << yearReleased << '\n'         // the print function
+             << "  Screenwriter: " << title << "\n\n";
+    }
+};
+
+int main() {
+    ifstream in("input.txt");                                       // Open the file
+    if (!in) {
+        cerr << "Could not open input.txt\n";                       // safety net for if file not open       
+        return 1;
+    }
+
+    vector<Movie> movies;
+    movies.reserve(4);
+
+    string title, yearStr, writer;
+    while (getline(in, title) && getline(in, yearStr) && getline(in, writer)) {
+        Movie tmp;                          // temporary Movie object
+        tmp.setTitle(title);
+        tmp.setYear(stoi(yearStr));         // convert year string to int (the stoi syntax)
+        tmp.setScreenWriter(writer);
+        movies.push_back(tmp);              // append to container
+
+        if (movies.size() == 4) break;      // store four records exactlllyyy
+    }
+}
