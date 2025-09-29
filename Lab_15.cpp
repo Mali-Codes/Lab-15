4607006953dc6a65eb8fa8d4f466448e7d9d08c8
Malakai <poncemalakai@gmail.com>
2025-09-28 21:05:38 -0700

    Commit 2 - building the class

--- Lab_15.cpp
+++ Lab_15.cpp
@@ -3,3 +3,19 @@
 #include <string>
 #include <vector>
 using namespace std;
+
+class Movie {
+private:
+    string screenWriter;
+    int yearReleased{};
+    string title;
+
+public:
+
+    void setTitle(const string& t)        { title = t; }            // Setters like lab 14
+    void setYear(int y)                   { yearReleased = y; }
+    void setScreenWriter(const string& w) { screenWriter = w; }
+
+    const string& getTitle() const        { return title; }         // getters readable private objects
+    int getYear() const                   { return yearReleased; }
+    const string& getScreenWriter() const
